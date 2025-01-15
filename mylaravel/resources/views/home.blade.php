4d0f5c11688fdc0a76828efb5e2e5c355c78967a
CarroTTo25 <carrotto25@gmail.com>
2025-01-15 17:18:57 +0700

    Laravel

---
+++ mylaravel/resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('Layouts.default')
+@section('head')
+<title>SE CAMP 66</title>
+@endsection
+@section('body')
+<h1>SE CAMP 66</h1>
+@endsection
+@section('be4_body')
+<h1>code before body</h1>
---
+++ mylaravel/routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MyController;
+
+Route::get('/mycontroller/{id?}',
+    [MyController::class,'myfunction']);
+
+Route::post('/mycontroller/{id?}',
+    [MyController::class,'myfunction']);
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('/hello/{id?}',
+function($val="")
+{
+    return "<h1>Hello world$val</h1>";
+});
