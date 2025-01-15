e12dfd528535255251980592318847c5100bda4a
CarroTTo25 <carrotto25@gmail.com>
2025-01-15 17:13:56 +0700

    laravel

--- mylaravel/resources/views/myview.blade.php
+++
@@ -1,9 +0,0 @@
-<h1> My Controller </h1>
-<h1><?php echo $myvalue; ?></h1>
-<h1>myinput : {{$myinput; }}</h1>
-<h1>myvalue : {{$myvalue; }}</h1>
-<form method = "post" action = "{{ url('/mycontroller') }}">
-    @csrf
-    <input type ="text" name = "myinput" >
-    <button type ="submit">Submit</button>
-</from>
