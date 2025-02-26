8ef8886fb6c75435ecc0c47b0be16327e8d00f66
CarroTTo25 <carrotto25@gmail.com>
2025-02-26 16:16:29 +0700

    ...

--- mylaravel/app/Http/Controllers/LoginController.php
+++ mylaravel/app/Http/Controllers/LoginController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class LoginController extends Controller
 {
     //
-    function index(){
-        return view ('login');
+    function index (){
+        return view('login');
+    }
+    function Login(Request $req){
+        $user = User::where('email', $req->email)->first();
+        if ($user && Hash::check($req->password, $user->password)) {
+            return redirect('/users');
+        } else {
+            return redirect('/login');
+
+        }
     }
 }
--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/login' ,
     [LoginController::class, 'index']);
+Route::post('/login' ,
+    [LoginController::class, 'login']);
 Route::get('/register' ,
     [RegisterController::class, 'index']);
 Route::post('/register',
